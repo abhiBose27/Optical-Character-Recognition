0df99ca47fa0ab809e0be3db1682bd32084b07ad
Abhishek <boseabhishek2@gmail.com>
2025-02-16 23:56:51 +0530

    [Model] Testing accuracy

--- OCR/TrainModel.c
+++ OCR/TrainModel.c
@@ -55,24 +55,32 @@ void train_model(Network* network, size_t nb_targets) {
     free_dataset(&tr_set);
 }
 
+void test_network(Network* network, size_t nb_targets) {
+    size_t prediction;
+    size_t nb_correct = 0;
+    Training_set tr_set = prepare_training_dataset(52);
+    shuffle_dataset(tr_set.data, tr_set.nb_data);
+    for (size_t i = 0; i < tr_set.nb_data; i++) {
+        if (!tr_set.data[i].image)
+            continue;
+        prediction = get_prediction(network, tr_set.data[i].image, nb_targets);
+        if (prediction == tr_set.data[i].target)
+            nb_correct += 1;
+    }
+    printf("Accuracy: %f\n", (double) nb_correct * 100 / tr_set.nb_data);
+    free_dataset(&tr_set);
+}
+
 int main(int argc, char* argv[]) {
-    srand (time(NULL));
-    Network network = generate_network(3, 784, 52);   
-    train_model(&network, 52);
-    save_network(&network);
-    free_network(&network);
+    //srand (time(NULL));
+    //Network network = generate_network(3, 784, 52);   
+    //train_model(&network, 52);
+    //save_network(&network);
+    //free_network(&network);
 
     
-    /* network = get_trained_network(3, 784, 52);
-    SDL_Surface* image = get_image("./Training_images4/7char.bmp");
-    double* inputs = get_image_to_pixel_intensity_matrix(image);
-    double* outputs = feed_forward(&network, inputs);
-    for (size_t j = 0; j < 52; j++) {
-        printf("%f: %ld\n", outputs[j], j);
-    }
-    free(outputs);
-    free(inputs);
-    SDL_FreeSurface(image);
-    free_network(&network); */
+    Network network = get_trained_network(3, 784, 52);
+    test_network(&network, 52);
+    free_network(&network);
     return 0;
 }
