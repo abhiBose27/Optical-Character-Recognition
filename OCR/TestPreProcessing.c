1e2c8bef1d1d5693acbb4934ab09cbd5e42ea07d
Abhishek <boseabhishek2@gmail.com>
2025-02-25 23:56:59 +0530

    [TestPreProcessing] Added an exec file to test pre processing

---
+++ OCR/TestPreProcessing.c
@@ -0,0 +1,20 @@
+#include <err.h>
+#include "sdl_libs.h"
+#include "PreProcessing.h"
+
+
+int main(int argc, char* argv[]) {
+    if (argc != 2)
+        errx(1, "Invalid number of arguments.\n");
+    if (init_SDL() == -1)
+        errx(1, "Could not initialise SDL: %s.\n", SDL_GetError());
+    SDL_Surface* image = get_image(argv[1]);
+    if (!image)
+        errx(1, "Cant get image from %s: %s", argv[1], SDL_GetError());
+    SDL_Surface* new_image = pre_processing(image);
+    display_image(new_image, 5000);
+    SDL_FreeSurface(image);
+    SDL_FreeSurface(new_image);
+    SDL_Quit();
+    return EXIT_SUCCESS;
+}
